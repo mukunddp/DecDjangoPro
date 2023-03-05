6aeb73a1e80b936d6c370bdbe49f5a2821512498
mukunddp <mukundparve98@gmail.com>
2023-03-05 18:01:08 +0530

    Again

--- TASocial/apps/social/urls.py
+++ TASocial/apps/social/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
@@ -9,5 +10,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('about/', views.about, name='about'),
-]
+    path('add_student/', views.add_student, name='add_student'),
+    path('store_student/', views.store_student, name='store_student'),
 
+]
--- TASocial/apps/social/views.py
+++ TASocial/apps/social/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Student
 
 # Create your views here.
 # def contact(request):
 #     return HttpResponse('''This is contact HTTP Response''')
 
+# CRUD
+# Create a Entry in DB
+# Read the Entries from DB
+# Update the Entries from DB
+# Delete the Entries from DB
+def store_student(request):
+    s = Student()
+    s.name_student = request.POST.get('student_name')
+    s.name_college = request.POST.get('clg_name')
+    s.department = request.POST.get('department')
+    s.year_pass = request.POST.get('year')
+    s.mobile = request.POST.get('mobile')
+    s.save()
+    return redirect('index')
+
+def add_student(request):
+    return render(request, 'add_student.html')
 
 def home(request):
     return render(request, 'index.html')
